1ecebc73a4ddc79b3282b74c030c752096b00706
shaismit <87336527+shaismit@users.noreply.github.com>
2021-07-24 21:38:04 +0300

    Update exercise2-fix.sh

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #add fix to exercise2 here
 
-#calling the site without using the "www"
-"curl -o 007.txt http://ascii-art.de/ascii/ab/007.txt
+#Remove wrong DNS translation
+sudo sed -i 's/127.0.0.1 www.ascii-art.de//g' /etc/hosts
+
+#I "curled" the site without using the "www".
+#After a secound thought I had to do something more sophisticated (like I to remove is from a wrong field\row from the host file ...:) 
+curl 007.txt http://ascii-art.de/ascii/ab/007.txt
 
 #Creating also a local file using "echo" command to insert the command exercise2-fix.sh
 echo "curl -o 007.txt http://ascii-art.de/ascii/ab/007.txt" >  exercise2-fix.sh
 
 #Change the exercise2-fix.sh file to executable file
 sudo chmod +x exercise2-fix.sh
-
-
-#Running the execute file 
- ~/./exercise1-fix.sh
