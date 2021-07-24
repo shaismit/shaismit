3ec66be7b8bb228db4e636e7fb4b756dd2847d01
shaismit <87336527+shaismit@users.noreply.github.com>
2021-07-24 21:30:55 +0300

    Update exercise1-fix.sh

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -4,8 +4,17 @@
 #...(HTTP, FTP, IMAP, POP3, SCP, SFTP, SMTP, TFTP, TELNET, LDAP or FILE)
 #curl command is using as a tool to transfer data to or from a server
 
+#shutdown -r 00
+#I ran the command route -n (after installing the net-tools by using apt-get net-tools)
+#I found the a strange route : 208.86.224.90   0.0.0.0 
+#I ping -a to this address (in my local computer):
+#Pinging www.textfiles.com [208.86.224.90]
+#And understand there something wrong with the routing table 
+#Then I remove the wrong route by using the command : 
+sudo route del -host www.textfiles.com
+
 #This right command should be : 
-curl -o bnbascii.txt http://www.textfiles.com/art/bnbascii.txt
+curl bnbascii.txt http://www.textfiles.com/art/bnbascii.txt
 
 #I created a script that will enable to run the the command manual in server1 
 
@@ -14,6 +23,3 @@ echo "curl -o bnbascii.txt http://www.textfiles.com/art/bnbascii.txt" >  exercis
 
 #Change the exercise1-fix.sh file to executable file
 sudo chmod +x exercise1-fix.sh
-
-#Running the execute file 
-#/home/vagrant/./exercise1-fix.sh
