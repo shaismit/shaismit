1c70eb827b6b1cfd9ae4c15c13b6e33f029016fa
shaismit <87336527+shaismit@users.noreply.github.com>
2021-07-24 15:12:47 +0300

    Update exercise2-fix.sh

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #add fix to exercise2 here
+
+#calling the site without using the "www"
+"curl -o 007.txt http://ascii-art.de/ascii/ab/007.txt
+
+#Creating also a local file using "echo" command to insert the command exercise2-fix.sh
 echo "curl -o 007.txt http://ascii-art.de/ascii/ab/007.txt" >  exercise2-fix.sh
+
+#Change the exercise2-fix.sh file to executable file
 sudo chmod +x exercise2-fix.sh
