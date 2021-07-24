ae4d5d2b0dd3b0bbd354a31d7e3d72d70f174b16
shaismit <87336527+shaismit@users.noreply.github.com>
2021-07-24 22:06:50 +0300

    Update exercise4-fix_server1.sh

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
+
+#We need to add the server2 ip translation because we don't have a DNS server
+#I tried this command & I got a permission denied 
+sudo echo "192.168.100.11 server2" >> /etc/hosts
+
+#So I seek for other command and found other command that help me to overcome the permission issue : 
+sudo -- sh -c -e "echo '192.168.100.11 server2' >> /etc/hosts"
+
+#sh is a command language interpreter that executes commands read from a command line string, the standard input, or a specified file
