7399db8b15390dba04d40bc77f67bef27c5f2688
shaismit <87336527+shaismit@users.noreply.github.com>
2021-07-24 14:25:02 +0300

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 #add fix to exercise3 here
+#Changing the permission of get in into the root link site from denying to granted by using the command "sed"
+#SED command in UNIX is stands for stream editor and it can perform lot’s of function on file like, searching, find and replace, insertion or deletion.
+
+$ sudo sed -i "s/denied/granted/g" /etc/apache2/sites-available/000-default.conf
+
+#Changing the message from "Hello world" to "Hello OpsSchool"
+$ sudo sed -i "s/Hello world/Hello OpsSchool/g" /var/www/html/index.html
+
+#Reset the Apache service that it'll take effect
+$ sudo systemctl restart apache2
