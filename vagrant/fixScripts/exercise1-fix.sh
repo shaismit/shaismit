1b4c01663db79b1aa314d20dd78793dc15886822
shaismit <87336527+shaismit@users.noreply.github.com>
2021-07-24 12:01:33 +0300

    Update exercise1-fix.sh

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #add fix to exercise1 here
-echo "curl -o bnbascii.txt http://www.textfiles.com/art/bnbascii.txt" >  exercise1-fix.sh
+#I add the 
+echo "curl bnbascii.txt http://www.textfiles.com/art/bnbascii.txt" >  exercise1-fix.sh
 sudo chmod +x exercise1-fix.sh
