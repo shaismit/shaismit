8001ca00cd17c863a9204936f85c58533c4fb352
shaismit <87336527+shaismit@users.noreply.github.com>
2021-07-24 13:07:10 +0300

    Update exercise1-fix.sh

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #add fix to exercise1 here
-curl bnbascii.txt http://www.textfiles.com/art/bnbascii.txt
-#I created also a script that will enable to run the the command manual if something went worng
-echo "curl bnbascii.txt http://www.textfiles.com/art/bnbascii.txt" >  exercise1-fix.sh
+curl -o bnbascii.txt http://www.textfiles.com/art/bnbascii.txt
+
+#I created a script that will enable to run the the command manual in server1 
+#curl is a command line tool to transfer data to or from a server, using any of the supported protocols
+#...(HTTP, FTP, IMAP, POP3, SCP, SFTP, SMTP, TFTP, TELNET, LDAP or FILE)
+#curl command is using as a tool to transfer data to or from a server
+
+#The "-o" display the process & make a local copy of bnbascii.txt
+echo "curl -o bnbascii.txt http://www.textfiles.com/art/bnbascii.txt" >  exercise1-fix.sh
+
+#I also add to the sh file executable option 
 sudo chmod +x exercise1-fix.sh
