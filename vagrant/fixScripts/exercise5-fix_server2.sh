f71f32cdc4d55ff8b33f1a4c9a3fee70595acfc5
shaismit <87336527+shaismit@users.noreply.github.com>
2021-07-30 15:38:56 +0300

    Update exercise5-fix_server2.sh

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+#Generate the private & public key in Server2 
+#We need to generate the keys under vagrant user in the guest machine
+#sudo -H : Request that the security policy set the HOME environment variable to the home directory
+#sudo -u : Run the command as a user other than the default target user
+#bash -c : -c  option  is  present,  then commands are read from the first non-option argument command string
+#echo -e : enable interpretation of backslash escapes
+#ssh-keygen : -q quite
+#ssh-keygen : -N Provides the new passphrase.
+
+#sudo -H -u vagrant bash -c 'echo -e "\n" | ssh-keygen -q -t rsa -N '' <<< ""$'\n'"y" > /dev/null 2>&1'
+sudo -H -u vagrant bash -c 'echo -e "\n" | ssh-keygen -q -N "" '
+
+
+#In server2 adding the public key
+curl http://192.168.100.10/pubkey.txt >> /home/vagrant/.ssh/authorized_keys
